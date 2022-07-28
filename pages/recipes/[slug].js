899230ff5daea567a6fa5437833b57b6363be89a
Azat <harutyunyanazat29@gmail.com>
2022-07-29 01:11:38 +0400

    Generating Paths

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,7 +1,43 @@
-export default function RecipeDetails() {
+import { createClient } from 'contentful'
+
+export default function RecipeDetails({ recipe }) {
+	console.log(recipe)
 	return (
 		<div>
 			Recipe Details
 		</div>
 	)
+}
+
+const client = createClient({
+	space: process.env.CONTENTFUL_SPACE_ID,
+	accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+})
+
+export const getStaticPaths = async () => {
+	const res = await client.getEntries({
+		content_type: 'recipe'
+	})
+
+	const paths = res.items.map( item => {
+		return {
+			params: { slug: item.fields.slug }
+		}
+	})
+
+	return {
+		paths,
+		fallback: false
+	}
+}
+
+export const getStaticProps = async ({ params }) => {
+	const { items } = await client.getEntries({
+		content_type: 'recipe',
+		'fields.slug': params.slug
+	})
+
+	return {
+		props: { recipe: items[0] }
+	}
 }
