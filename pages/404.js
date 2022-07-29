94690005b235627d98b3fdd22213b64e4c6a3394
Azat <harutyunyanazat29@gmail.com>
2022-07-29 16:22:30 +0400

    cannot

--- pages/404.js
+++ pages/404.js
@@ -14,7 +14,7 @@ export default function NotFound() {
 	return (
 		<div className="not-found">
 			<h1>404</h1>
-			<h2>Ooops! That page cannopt be found :(</h2>
+			<h2>Ooops! That page cannot be found :(</h2>
 			<p>Redirecting to the <Link href="/">Homepage</Link> for more marmite goodness...</p>
 
 			<style jsx>{`
